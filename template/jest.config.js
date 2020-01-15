dbb9bcda42db562d851631611bd35b1adb1edf45
张雄军 <zxj@blibao.cn>
2020-01-15 16:14:49 +0800

    perf: 依赖包升级

--- template/jest.config.js
+++ template/jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
+  // 与 vue.config.js 同步的 alias 别名
+  moduleNameMapper: {
+    '^{{#if namespace}}{{namespace}}/{{/if}}{{ name }}(.*)$': '<rootDir>/packages/$1'
+  },
+  // 需要转换的 esm 例外模块（默认不转换所有 node_modules 中的模块）
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!lodash-es)']
 };
